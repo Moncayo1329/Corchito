f3185335c6cce046272249265f03ba198db0e344
Moncayo1329 <moncayomichael23@gmail.com>
2025-09-24 23:22:14 +0100

    making:listar usuarios and ver usuarios

--- corchito-backend/src/main.rs
+++ corchito-backend/src/main.rs
@@ -1,11 +1,37 @@
 mod routes; // importa la carpeta routes, le dice a rust que hay un modulo llamado routes que contiene otros archivos.
+use routes::usuario;
 
 fn main(){
-    println!("Servidor corchito iniciando...")
+    println!("Servidor corchito iniciando...");
 
+
+
+    // 1. Probar listar usuarios.
+
+    let usuarios = usuario::listar_usuarios();
+    println!("Lista de usuarios:");
+    for u in &usuarios {
+
+        println!("{} - {} ({})", u.id, u.nombre, u.email);
+    }
+
+    // 2. Probar ver un usuario especifico
+
+    let resultado = usuario::ver_usuario(1);
+
+    match resultado {
+        Some(u) => {
+
+            println!("Usuario encontrado: {} - {} ({})", u.id, u.nombre, u.email)
+        }
+    
+    None => {
+        println!("Usuario no encontrado")
+    }
+}
 
     // simulamos llamadas a rutas 
-    routes::usuario::listar_usuarios(); // llamado a la funcion que definimos en usuario.rs
-    routes::usuario::ver_usuario(1);
+    //routes::usuario::listar_usuarios(); // llamado a la funcion que definimos en usuario.rs
+    //routes::usuario::ver_usuario(1);
 }
 
--- corchito-backend/src/routes/usuario.rs
+++ corchito-backend/src/routes/usuario.rs
@@ -13,13 +13,28 @@ pub struct Usuario {
 // significa que son funciones publicas, para poder llamarlas desde el main.rs
 // id;u32 simulamos un parametro que vendria de la url.
 
-pub fn listar_usuarios() {
-    println!("Esta funcion listaria todos los nuevos usuarios");
+// paso 2 crear un vector de usuarios de prueba y devolver resultados reales en vez de solo printil!
+
+pub fn listar_usuarios() -> Vec<Usuario> {
+    vec![
+  Usuario{ 
+id:1, 
+nombre:"Michael".to_string(),
+email: "moncayomichael23@gmail.com".to_string(),
+password:"1234".to_string(),
+  },
+
+  Usuario {id:2, 
+ nombre:"Ana".to_string(),
+email:"anabel@gmail.com".to_string(),
+password:"abdb".to_string(),
+},
+    ]
 }
 
-pub fn ver_usuario(id: u32){
+pub fn ver_usuario(id: u32) -> Option<Usuario>{
 
-    println!("Esta funcion mostraria el usuario com id:{}", id);
-
+let usuarios = listar_usuarios();
+usuarios.into_iter().find(|u| u.id == id)
 }
 
